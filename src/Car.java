e6bb3b8706241eb487ff5094e6fde261578e207f
GrantDit <kiruha5854@mail.ru>
2022-12-20 21:36:43 +0300

    Задание 3 "Содание автомобиля"

--- src/Car.java
+++ src/Car.java
@@ -8,12 +8,31 @@ public class Car {
 
     Car(String brand, String model, double engineVolume, String color, int year, String country) {
         this.brand = brand;
+        if (this.brand == null || this.brand.isBlank()) {
+            this.brand = "Информация не указана";
+        }
         this.model = model;
+        if (this.model == null || this.model.isBlank()) {
+            this.model = "Информация не указана";
+        }
         this.engineVolume = engineVolume;
+        if (this.engineVolume <= 0) {
+            throw new IllegalArgumentException("Информация указана не верно");
+        }
         this.color = color;
+        if (this.color == null || this.color.isBlank()) {
+            this.color = "Информация не указана";
+        }
         this.year = year;
+        if (this.year <= 0) {
+            throw new IllegalArgumentException("Информация указана не верно");
+        }
         this.country = country;
+        if (this.country == null || this.country.isBlank()) {
+            this.country = "Информация не указана";
+        }
     }
+
 
     public String toString() {
         return " Марка автомобиля: " + brand + ",модель автомобиля: " + model + ",объем двигателя: " + engineVolume + ",цвет кузова: " + color + ",год производства: " + year + ",страна  производства: " + country;
